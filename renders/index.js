63ef81509334d39543eed4e04f4d7fe697610752
Eunji Bae <creative.apme@gmail.com>
2022-01-12 16:42:53 +0900

    eunji: admin page setting

--- renders/index.js
+++ renders/index.js
@@ -9,6 +9,74 @@ router.get("/", (req, res, next) => {
     }
 });
 
+router.get("/giftStatistics", (req, res, next) => {
+    try {
+        res.render("./statistics/giftStatistics");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/menuStatistics", (req, res, next) => {
+    try {
+        res.render("./statistics/menuStatistics");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/giftQuestionsList", (req, res, next) => {
+    try {
+        res.render("./gifts/giftQuestionsList");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/giftList", (req, res, next) => {
+    try {
+        res.render("./gifts/giftList");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/menuList", (req, res, next) => {
+    try {
+        res.render("./menus/menuList");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/moneyQuestionsList", (req, res, next) => {
+    try {
+        res.render("./money/moneyQuestionsList");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+router.get("/boardList", (req, res, next) => {
+    try {
+        res.render("./boards/boardList");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
+
+
+
+
+router.get("/login", (req, res, next) => {
+    try {
+        res.render("login");
+    } catch (error) {
+        res.render("error");
+    }
+});
+
 router.get("/error", (req, res, next) => {
     try {
         res.render("error");
