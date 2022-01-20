63ba8a75ccd8c09c57906bbfe675f4b1ad7f242d
Eunji Bae <creative.apme@gmail.com>
2022-01-20 19:19:08 +0900

    eunji: 축의금 CSV 적용

--- public/js/moneyQuestionsList.js
+++ public/js/moneyQuestionsList.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
     getMoneyQuestionList();
 });
 
+let newRows = [];
 function getMoneyQuestionList() {
     const datatablesSimple = document.getElementById("datatablesSimple");
     const dataTable = new simpleDatatables.DataTable(datatablesSimple);
@@ -11,10 +12,7 @@ function getMoneyQuestionList() {
         url: `/api/moneyQuestions`,
         data: {},
         success: (response) => {
-            const allMoneyQuestions = response;
-            let newRows = [];
-
-            console.log(allMoneyQuestions);
+            const allMoneyQuestions = response;         
 
             for (let i = 0; i < allMoneyQuestions.length; i++) {
                 let tempRow = [];
@@ -59,4 +57,58 @@ function deleteItem(id) {
             history.go(0);
         },
     });
+}
+
+// CSV 추출
+let columData = [];
+function exportExcel() {
+    let selectedColum = [];
+
+    // 데이터인 newRows만큼 추출
+    for (let i = 0; i < newRows.length; i++) {
+        // 필요한 컬럼의 데이터만 추출해서 배열로 만듬
+        for (let j = 0; j < 5; j++) {
+            // 처음에 th(컬럼명) 부분 먼저 추출
+            if (i == 0 && j == 0) {
+                selectedColum.push(document.getElementsByTagName('th')[0].innerText);
+                selectedColum.push(document.getElementsByTagName('th')[1].innerText);
+                selectedColum.push(document.getElementsByTagName('th')[2].innerText);
+                selectedColum.push(document.getElementsByTagName('th')[3].innerText);
+                selectedColum.push(document.getElementsByTagName('th')[4].innerText);
+            }
+            selectedColum.push(newRows[i][j])
+        }
+    }
+    // 각 행의 데이터를 하나의 배열에 각각 담음 (행의 길이 만큼 배열 생성)
+    for (i = 0; i < selectedColum.length; i += 5) {
+        columData.push(selectedColum.slice(i, i + 5));
+    }
+
+    const wb = XLSX.utils.book_new();
+    const newWorksheet = excelHandler.getWorksheet();
+    XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
+    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), excelHandler.getExcelFileName());
+}
+
+const excelHandler = {
+    getExcelFileName: function () {
+        return 'sasohae-gift-data.xlsx';
+    },
+    getSheetName: function () {
+        return 'giftList Sheet';
+    },
+    getExcelData: function () {
+        return columData;
+    },
+    getWorksheet: function () {
+        return XLSX.utils.aoa_to_sheet(this.getExcelData());
+    }
+}
+
+function s2ab(s) {
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
 }
