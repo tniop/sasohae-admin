259a7725e458a0b3f91396a8622c195ec361641f
Seongjin Lee <babo318@gmail.com>
2022-01-21 16:23:46 +0900

    layoutHeader: 누적 방문자수 집계 표시

--- public/js/layoutHeader.js
+++ public/js/layoutHeader.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    getToTalVisitorCnt();
+});
+
 function logOut() {
     if (window.confirm("로그아웃 하시겠습니까?")) {
         sessionStorage.clear();
@@ -8,4 +12,21 @@ function logOut() {
 function moveMyPage() {
     const admin_id = sessionStorage.getItem("_id");
     location.href = `/mypage/${admin_id}`;
+}
+
+function getToTalVisitorCnt() {
+    const selector = document.querySelector("#totVisitorCnt");
+
+    $.ajax({
+        type: "get",
+        url: `/api/statistics`,
+        data: {},
+        success: (res) => {
+            const totVisitorCnt = res[0].totVisitorCnt;
+            selector.innerText = `누적방문자수: ${totVisitorCnt}명`;
+        },
+        error: (err) => {
+            alert("방문자수 조회에 실패하였니다!");
+        },
+    });
 }
