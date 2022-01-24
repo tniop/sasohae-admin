bfc7e7d12df72baf55b955e54b28912a07048b2e
Eunji Bae <creative.apme@gmail.com>
2022-01-24 13:11:49 +0900

    eunji: gifts 2차 중복코드 수정

--- public/js/gifts.js
+++ public/js/gifts.js
@@ -1,3 +1,73 @@
+function insertInfo() {
+    const giftName = $("#giftName").val();
+    let giftTarget = [];
+    let giftEvent = [];
+    let age = [];
+    let sex = "";
+    const giftAnswerExpensive = $("#expensive").val();
+    const giftAnswerPersonality = $("#personality").val();
+    const giftAnswerEmotional = $("#emotional").val();
+    const giftAnswerTrendy = $("#trendy").val();
+    const formData = new FormData();
+    const img = document.getElementById("imgFile").files[0];
+
+    giftList(giftTarget, "giftTarget");
+    giftList(giftEvent, "giftEvent");
+    giftList(age, "age");
+
+    if ($("#giftName").val() == "") {
+        alert("선물이름을 입력해 주세요!");
+        $("#giftName").focus();
+        return;
+    }
+
+    if ($("#imgFile").val() == "") {
+        alert("이미지 파일을 첨부해 주세요!");
+        return;
+    }
+
+    if (giftTarget.length == 0 || giftEvent.length == 0 || age.length == 0) {
+        alert("체크박스를 체크해 주세요!")
+        return;
+    }
+
+    const radioLength = document.getElementsByName("sexRadios").length;
+    for (let i = 0; i < radioLength; i++) {
+        if (document.getElementsByName("sexRadios")[i].checked == true) {
+            sex = (document.getElementsByName("sexRadios")[i].value);
+        }
+    }
+
+    formData.append("giftName", giftName);
+    formData.append("img", img);
+    formData.append("giftTarget", JSON.stringify(giftTarget));
+    formData.append("giftEvent", JSON.stringify(giftEvent));
+    formData.append("sex", sex);
+    formData.append("age", JSON.stringify(age));
+    formData.append("giftAnswerExpensive", giftAnswerExpensive);
+    formData.append("giftAnswerPersonality", giftAnswerPersonality);
+    formData.append("giftAnswerEmotional", giftAnswerEmotional);
+    formData.append("giftAnswerTrendy", giftAnswerTrendy);
+
+    if (window.confirm("선물을 등록 하시겠습니까?")) {
+        $.ajax({
+            type: "POST",
+            url: "/api/gifts",
+            traditional: true,
+            contentType: false,
+            processData: false,
+            data: formData,
+            success: function (response) {
+                alert("선물 등록에 성공하였습니다!")
+                location.href = "/giftList";
+            },
+            error: function (error) {
+                alert(error.responseJSON.errorMessage)
+            },
+        });
+    }
+}
+
 function getSelectedGift() {
     const gift_id = window.location.href.split("/")[4];
     $("#giftList").empty()
@@ -13,7 +83,6 @@ function getSelectedGift() {
             location.href = "/giftList";
         },
         success: function (selectedGift) {
-            // 질문 타입 radio 입력
             const name = selectedGift.giftName;
             const url = selectedGift.giftUrl;
             const target = selectedGift.giftTarget;
@@ -35,7 +104,6 @@ function getSelectedGift() {
                                     </div>`
             $("#imgFile").append(htmlTemp)
 
-            // 선물타겟 체크박스
             for (let i = 0; i < target.length; i++) {
                 const value = ["1", "2", "3", "4", "5", "6", "7", "8"]
                 for (let j = 0; j < value.length; j++) {
@@ -46,7 +114,7 @@ function getSelectedGift() {
                     }
                 }
             }
-            // 선물목적 체크박스
+
             for (let i = 0; i < event.length; i++) {
                 const value = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
                 for (let j = 0; j < value.length; j++) {
@@ -57,7 +125,7 @@ function getSelectedGift() {
                     }
                 }
             }
-            // 성별
+
             if (sex == "*") {
                 $("#sexRadiosAll").prop("checked", true);
             } else if (sex == "M") {
@@ -65,7 +133,7 @@ function getSelectedGift() {
             } else if (sex == "W") {
                 $("#sexRadios2").prop("checked", true);
             }
-            // 연령대
+
             for (let i = 0; i < age.length; i++) {
                 const value = ["20", "30", "40", "50", "60"]
                 for (let j = 0; j < value.length; j++) {
@@ -76,187 +144,35 @@ function getSelectedGift() {
                     }
                 }
             }
-            
-            /*
-          // 설문에 대한 답변 부분
-          const surveryAnswerList = ["expensive", "personality", "emotional", "trendy"]
-          for (let i = 0; i < surveryAnswerList.length; i++) {
-              if (surveryAnswerList[i] == "*") {
-                  $("#" + surveryAnswerList[i]).val("*", true);
-              } else if (surveryAnswerList[i] == "T") {
-                  $("#" + surveryAnswerList[i]).val("T", true);
-              } else if (surveryAnswerList[i] == "F") {
-                  $("#" + surveryAnswerList[i]).val("F", true);
-              }
-              console.log($("#" + surveryAnswerList[i]).val());
-          }
-          본 코드에 문제 있어서 아래 처럼 일단 수정해둠
-          */
-
-            // Expensive
-            if (expensive == "*") {
-                $("#expensive").val("*", true);
-            } else if (expensive == "T") {
-                $("#expensive").val("T", true);
-            } else if (expensive == "F") {
-                $("#expensive").val("F", true);
-            }
-            // Personality
-            if (personality == "*") {
-                $("#personality").val("*", true);
-            } else if (personality == "T") {
-                $("#personality").val("T", true);
-            } else if (personality == "F") {
-                $("#personality").val("F", true);
-            }
-            // Emotional
-            if (emotional == "*") {
-                $("#emotional").val("*", true);
-            } else if (emotional == "T") {
-                $("#emotional").val("T", true);
-            } else if (emotional == "F") {
-                $("#emotional").val("F", true);
-            }
-            // Trendy
-            if (trendy == "*") {
-                $("#trendy").val("*", true);
-            } else if (trendy == "T") {
-                $("#trendy").val("T", true);
-            } else if (trendy == "F") {
-                $("#trendy").val("F", true);
-            }
-            
+            $("#expensive").val(`${expensive}`, true);
+            $("#personality").val(`${personality}`, true);
+            $("#emotional").val(`${emotional}`, true);
+            $("#trendy").val(`${trendy}`, true);
         }
     });
 }
 
 function reviseInfo() {
     const gift_id = window.location.href.split("/")[4];
-
-    if (confirm("수정하시겠습니까?") == true) {
-    } else {
-        return false;
-    }
-
     const giftTarget = [];
-    const giftTargetList = document.getElementsByName("giftTarget");
-    giftTargetList.forEach((node) => {
-        if (node.checked) {
-            giftTarget.push(node.value);
-        }
-    });
-
     const giftEvent = [];
-    const giftEventList = document.getElementsByName("giftEvent");
-    giftEventList.forEach((node) => {
-        if (node.checked) {
-            giftEvent.push(node.value);
-        }
-    });
-
     const age = [];
-    const ageList = document.getElementsByName("age");
-    ageList.forEach((node) => {
-        if (node.checked) {
-            age.push(node.value);
-        }
-    });
-    
-    if (giftTarget.length == 0 || giftEvent.length == 0 || age.length == 0) {
-        alert("체크박스를 체크해 주세요!")
-        return;
-    }
-
-    // 성별
     let sex = "";
-    const radioLength = document.getElementsByName("sexRadios").length;
-    for (let i = 0; i < radioLength; i++) {
-        if (document.getElementsByName("sexRadios")[i].checked == true) {
-            sex = (document.getElementsByName("sexRadios")[i].value);
-        }
-    }
 
     expensive = $("#expensive").val();
     personality = $("#personality").val();
     emotional = $("#emotional").val();
     trendy = $("#trendy").val();
 
-    $.ajax({
-        type: "PUT",
-        url: `/api/gifts/${gift_id}`,
-        traditional: true,
-        data: {
-            giftTarget: giftTarget,
-            giftEvent: giftEvent,
-            sex: sex,
-            age: age,
-            giftAnswerExpensive: expensive,
-            giftAnswerPersonality: personality,
-            giftAnswerEmotional: emotional,
-            giftAnswerTrendy: trendy,
-        },
-        success: function (response) {
-            alert("성공적으로 변경되었습니다.")
-            location.href = location.href
-            history.go(0);
-        },
-        error: function (error) {
-            alert(error.responseJSON.errorMessage)
-        },
-    });
-}
-
-
-/* giftInsert */
-function insertInfo() {
-    if (confirm("등록하시겠습니까?") == true) {
-    } else {
-        return false;
-    }
-
-    const giftName = $("#giftName").val();
-    if ($("#giftName").val() == "") {
-        alert("선물이름을 입력해 주세요!");
-        $("#giftName").focus();
-        return;
-    }
-
-    if ($("#imgFile").val() == "") {
-        alert("이미지 파일을 첨부해 주세요!");
-        return;
-    }
-  
-    const giftTarget = [];
-    const giftTargetList = document.getElementsByName("giftTarget");
-    giftTargetList.forEach((node) => {
-        if (node.checked) {
-            giftTarget.push(node.value);
-        }
-    });
-
-    const giftEvent = [];
-    const giftEventList = document.getElementsByName("giftEvent");
-    giftEventList.forEach((node) => {
-        if (node.checked) {
-            giftEvent.push(node.value);
-        }
-    });
-
-    const age = [];
-    const ageList = document.getElementsByName("age");
-    ageList.forEach((node) => {
-        if (node.checked) {
-            age.push(node.value);
-        }
-    });
+    giftList(giftTarget, "giftTarget");
+    giftList(giftEvent, "giftEvent");
+    giftList(age, "age");
 
     if (giftTarget.length == 0 || giftEvent.length == 0 || age.length == 0) {
         alert("체크박스를 체크해 주세요!")
         return;
     }
-    
-    // 성별
-    let sex = "";
+
     const radioLength = document.getElementsByName("sexRadios").length;
     for (let i = 0; i < radioLength; i++) {
         if (document.getElementsByName("sexRadios")[i].checked == true) {
@@ -264,109 +180,97 @@ function insertInfo() {
         }
     }
 
-    // 설문에 대한 답변들
-    const giftAnswerExpensive = $("#expensive").val();
-    const giftAnswerPersonality = $("#personality").val();
-    const giftAnswerEmotional = $("#emotional").val();
-    const giftAnswerTrendy = $("#trendy").val();
-
-    const formData = new FormData();
-    const img = document.getElementById("imgFile").files[0];
-
-    // formData에 담음. DB에 배열로 담기는 부분 JSON.stringify 처리
-    formData.append("giftName", giftName);
-    formData.append("img", img);
-    formData.append("giftTarget", JSON.stringify(giftTarget));
-    formData.append("giftEvent", JSON.stringify(giftEvent));
-    formData.append("sex", sex);
-    formData.append("age", JSON.stringify(age));
-    formData.append("giftAnswerExpensive", giftAnswerExpensive);
-    formData.append("giftAnswerPersonality", giftAnswerPersonality);
-    formData.append("giftAnswerEmotional", giftAnswerEmotional);
-    formData.append("giftAnswerTrendy", giftAnswerTrendy);
-
-    const body = {
-        giftName,
-        img,
-        giftTarget,
-        giftEvent,
-        sex,
-        age,
-        giftAnswerExpensive,
-        giftAnswerPersonality,
-        giftAnswerEmotional,
-        giftAnswerTrendy,
+    if (window.confirm("선물을 수정 하시겠습니까?")) {
+        $.ajax({
+            type: "PUT",
+            url: `/api/gifts/${gift_id}`,
+            traditional: true,
+            data: {
+                giftTarget: giftTarget,
+                giftEvent: giftEvent,
+                sex: sex,
+                age: age,
+                giftAnswerExpensive: expensive,
+                giftAnswerPersonality: personality,
+                giftAnswerEmotional: emotional,
+                giftAnswerTrendy: trendy,
+            },
+            success: function (response) {
+                alert("성공적으로 변경되었습니다.")
+                location.href = location.href
+                history.go(0);
+            },
+            error: function (error) {
+                alert(error.responseJSON.errorMessage)
+            },
+        });
     }
-    $.ajax({
-        type: "POST",
-        url: "/api/gifts",
-        data: formData,
-        traditional: true,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-            alert("선물 등록에 성공하였습니다!")
-            location.href = "/giftList";
-        },
-        error: function (error) {
-            alert(error.responseJSON.errorMessage)
-        },
-    });
+}
 
+function giftList(arr, name) {
+    const giftList = document.getElementsByName(name);
+    giftList.forEach((node) => {
+        if (node.checked) {
+            arr.push(node.value);
+        }
+    });
+}
+
+function IsCheckAllChecked(name) {
+    const giftType = document.getElementsByName(name);
+
+    if (giftType[0].checked) {
+        for (let i = 1; i < giftType.length; i++) {
+            giftType[i].checked = false;
+            giftType[i].disabled = true;
+        }
+    } else {
+        for (let i = 1; i < giftType.length; i++) {
+            giftType[i].disabled = false;
+        }
+    }
+}
+
+function chkboxAllChecked(name) {
+    const giftTarget = document.getElementsByName("giftTarget");
+    const giftEvent = document.getElementsByName("giftEvent");
+    const age = document.getElementsByName("age");
+
+    if (giftTarget[1].checked &&
+        giftTarget[2].checked &&
+        giftTarget[3].checked &&
+        giftTarget[4].checked &&
+        giftTarget[5].checked &&
+        giftTarget[6].checked &&
+        giftTarget[7].checked) {
+        giftTarget[0].checked = true;
+        IsCheckAllChecked(name)
+    }
+
+    if (giftEvent[1].checked &&
+        giftEvent[2].checked &&
+        giftEvent[3].checked &&
+        giftEvent[4].checked &&
+        giftEvent[5].checked &&
+        giftEvent[6].checked &&
+        giftEvent[7].checked &&
+        giftEvent[8].checked) {
+        giftEvent[0].checked = true;
+        IsCheckAllChecked(name)
+    }
+
+    if (
+        age[1].checked &&
+        age[2].checked &&
+        age[3].checked &&
+        age[4].checked &&
+        age[5].checked
+    ) {
+        age[0].checked = true;
+        IsCheckAllChecked(name)
+    }
 }
 
 function backToList() {
     location.href = "/giftList";
-}
-
-
-
-// /* insert, detail 페이지 공통 사용 */
-// 선물타겟 체크박스를 전체선택한 경우
-function targetAllChkClicked() {
-    $("#giftTargetCheckboxAll").click(function () {
-        const length = $(".targetChk").size();
-        if ($("#giftTargetCheckboxAll").is(":checked", true)) {
-            for (let i = 0; i < length; i++) {
-                $("#giftTargetCheckbox" + i).prop("checked", false)
-                $("#giftTargetCheckbox" + i).prop("disabled", true);
-            }
-        } else {
-            for (let i = 0; i < length; i++) {
-                $("#giftTargetCheckbox" + i).prop("disabled", false);
-            }
-        }
-    });
-}
-// // 선물목적 체크박스를 전체선택한 경우
-function eventAllChkClicked() {
-    $("#giftEventCheckboxAll").click(function () {
-        const length = $(".eventChk").size();
-        if ($("#giftEventCheckboxAll").is(":checked", true)) {
-            for (let i = 0; i < length; i++) {
-                $("#giftEventCheckbox" + i).prop("checked", false)
-                $("#giftEventCheckbox" + i).prop("disabled", true);
-            }
-        } else {
-            for (let i = 0; i < length; i++) {
-                $("#giftEventCheckbox" + i).prop("disabled", false);
-            }
-        }
-    });
-}
-// // 연령대 체크박스를 전체선택한 경우
-function ageAllChkClicked() {
-    $("#ageCheckboxAll").click(function () {
-        const length = $(".ageChk").size();
-        if ($("#ageCheckboxAll").is(":checked", true)) {
-            for (let i = 0; i < length; i++) {
-                $("#ageCheckbox" + i).prop("checked", false);
-                $("#ageCheckbox" + i).prop("disabled", true);
-            }
-        } else {
-            for (let i = 0; i < length; i++) {
-                $("#ageCheckbox" + i).prop("disabled", false);
-            }
-        }
-    });
 }
