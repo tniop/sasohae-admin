b155e68c7660713bae91a2cd3a36fe604d58c958
Seongjin Lee <babo318@gmail.com>
2022-01-24 22:32:40 +0900

    관리 페이지: guest일경우 권한 없음 기능 추가

--- public/js/giftList.js
+++ public/js/giftList.js
@@ -1,42 +1,8 @@
 $(document).ready(() => {
-    getGiftList();
+    checkAdminPosition();
 });
 
 let newRows = [];
-function getGiftList() {
-    const datatablesSimple = document.getElementById("datatablesSimple");
-    const dataTable = new simpleDatatables.DataTable(datatablesSimple);
-    $.ajax({
-        type: "GET",
-        url: `/api/gifts/`,
-        data: {},
-        success: (res) => {
-            const allGifts = res;
-
-            for (let i = 0; i < allGifts.length; i++) {
-                let tempRow = [];
-                tempRow.push(`${i+1}`);
-                tempRow.push(allGifts[i].giftName);
-                tempRow.push(`<img width="80px" src="${allGifts[i].giftUrl}">`);
-                tempRow.push(allGifts[i].giftRecommendCnt.toString());
-                tempRow.push(allGifts[i].giftLikeCnt.toString());
-                tempRow.push(`<input type="button" id="${allGifts[i].gift_id}" onclick="location.href='/gifts/${allGifts[i].gift_id}'"
-                class="btn btn-outline-primary" value="상세페이지">`);
-                tempRow.push(
-                    `<img src="../public/assets/img/deleteBtn.png" id="delete${allGifts[i].gift_id}" onClick="deleteItem(this.id)" width="40px" style="cursor:pointer;">`
-                );
-
-                newRows.push(tempRow);
-            }
-
-            dataTable.rows().add(newRows);
-        },
-    });
-}
-
-function moveToInsert() {
-    location.href = "/giftInsert";
-}
 
 function deleteItem(id) {
     const gift_id = id.split("delete")[1];
@@ -67,20 +33,30 @@ function exportExcel() {
         // 필요한 컬럼의 데이터만 추출해서 배열로 만듬
         for (let j = 0; j < 5; j++) {
             // 처음에 th(컬럼명) 부분 먼저 추출
-            if (i == 0 && j == 0) { 
-                selectedColum.push(document.getElementsByTagName('th')[0].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[1].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[2].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[3].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[4].innerText);
+            if (i == 0 && j == 0) {
+                selectedColum.push(
+                    document.getElementsByTagName("th")[0].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[1].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[2].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[3].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[4].innerText
+                );
             }
-            // url 컬럼일 때 
-            if (j == 2) { 
+            // url 컬럼일 때
+            if (j == 2) {
                 // img url 에서 src 내부 url만 추출
-                selectedColum.push((newRows[i][j]).split('"')[3]);
+                selectedColum.push(newRows[i][j].split('"')[3]);
             } else {
-                selectedColum.push(newRows[i][j]);    
-            }        
+                selectedColum.push(newRows[i][j]);
+            }
         }
     }
     // 각 행의 데이터를 하나의 배열에 각각 담음 (행의 길이 만큼 배열 생성)
@@ -91,28 +67,118 @@ function exportExcel() {
     const wb = XLSX.utils.book_new();
     const newWorksheet = excelHandler.getWorksheet();
     XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
-    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), excelHandler.getExcelFileName());
+    const wbout = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+    saveAs(
+        new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
+        excelHandler.getExcelFileName()
+    );
 }
 
 const excelHandler = {
     getExcelFileName: function () {
-        return 'sasohae-gift-data.xlsx';
+        return "sasohae-gift-data.xlsx";
     },
     getSheetName: function () {
-        return 'giftList Sheet';
+        return "giftList Sheet";
     },
     getExcelData: function () {
         return columData;
     },
     getWorksheet: function () {
         return XLSX.utils.aoa_to_sheet(this.getExcelData());
-    }
-}
+    },
+};
 
 function s2ab(s) {
-    const buf = new ArrayBuffer(s.length); 
-    const view = new Uint8Array(buf);  
-    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
     return buf;
+}
+
+function checkAdminPosition() {
+    const admin_id = sessionStorage.getItem("_id");
+
+    $.ajax({
+        type: "get",
+        url: `/api/admins/${admin_id}`,
+        data: {},
+        success: (response) => {
+            const adminPosition = response.adminPosition;
+            makeTable(adminPosition);
+        },
+        error: (err) => {
+            alert(err.responseJSON.errorMessage);
+        },
+    });
+}
+
+function makeTable(adminPosition) {
+    const datatablesSimple = document.getElementById("datatablesSimple");
+    const dataTable = new simpleDatatables.DataTable(datatablesSimple);
+    $.ajax({
+        type: "GET",
+        url: `/api/gifts/`,
+        data: {},
+        success: (res) => {
+            const allGifts = res;
+
+            for (let i = 0; i < allGifts.length; i++) {
+                let tempRow = [];
+                if (adminPosition != "guest") {
+                    tempRow.push(`${i + 1}`);
+                    tempRow.push(allGifts[i].giftName);
+                    tempRow.push(
+                        `<img width="80px" src="${allGifts[i].giftUrl}">`
+                    );
+                    tempRow.push(allGifts[i].giftRecommendCnt.toString());
+                    tempRow.push(allGifts[i].giftLikeCnt.toString());
+                    tempRow.push(`<input type="button" id="${allGifts[i].gift_id}" onclick="location.href='/gifts/${allGifts[i].gift_id}'"
+                                    class="btn btn-outline-primary" value="상세페이지">`);
+                    tempRow.push(
+                        `<img src="../public/assets/img/deleteBtn.png" id="delete${allGifts[i].gift_id}" onClick="deleteItem(this.id)" width="40px" style="cursor:pointer;">`
+                    );
+                } else {
+                    tempRow.push(`${i + 1}`);
+                    tempRow.push(allGifts[i].giftName);
+                    tempRow.push(
+                        `<img width="80px" src="${allGifts[i].giftUrl}">`
+                    );
+                    tempRow.push(allGifts[i].giftRecommendCnt.toString());
+                    tempRow.push(allGifts[i].giftLikeCnt.toString());
+                    tempRow.push(`<input type="button" id="${allGifts[i].gift_id}" onclick="alert('권한이 필요한 작업입니다!')"
+                                    class="btn btn-outline-primary" value="상세페이지">`);
+                    tempRow.push(
+                        `<img src="../public/assets/img/deleteBtn.png" id="delete${allGifts[i].gift_id}" onClick="alert('권한이 필요한 작업입니다!')" width="40px" style="cursor:pointer;">`
+                    );
+                }
+
+                newRows.push(tempRow);
+            }
+
+            dataTable.rows().add(newRows);
+        },
+    });
+}
+
+function moveToInsert() {
+    const admin_id = sessionStorage.getItem("_id");
+
+    $.ajax({
+        type: "get",
+        url: `/api/admins/${admin_id}`,
+        data: {},
+        success: (response) => {
+            const adminPosition = response.adminPosition;
+
+            if (adminPosition != "guest") {
+                location.href = "/giftInsert";
+            } else {
+                alert("권한이 필요한 작업입니다!");
+            }
+        },
+        error: (err) => {
+            alert(err.responseJSON.errorMessage);
+        },
+    });
 }
--- public/js/menuList.js
+++ public/js/menuList.js
@@ -1,42 +1,8 @@
 $(document).ready(() => {
-    getMenuList();
+    checkAdminPosition();
 });
 
 let newRows = [];
-function getMenuList() {
-    const datatablesSimple = document.getElementById("datatablesSimple");
-    const dataTable = new simpleDatatables.DataTable(datatablesSimple);
-
-    $.ajax({
-        type: "get",
-        url: `/api/menus`,
-        data: {},
-        success: (res) => {
-            const allMenus = res;
-        
-            for (let i = 0; i < allMenus.length; i++) {
-                let tempRow = [];
-                tempRow.push(`${i + 1}`);
-                tempRow.push(allMenus[i].menuName);
-                tempRow.push(`<img width="37px" src="${allMenus[i].menuUrl}">`);
-                tempRow.push(allMenus[i].menuRecommendCnt.toString());
-                tempRow.push(allMenus[i].menuLikeCnt.toString());
-                tempRow.push(`<input type="button" id="${allMenus[i].menu_id}" onClick="moveToDetail(this.id)"
-                class="btn btn-outline-primary" value="상세페이지">`);
-                tempRow.push(
-                    `<img style="cursor:pointer;" src="../public/assets/img/deleteBtn.png" id="${allMenus[i].menu_id}" onClick="deleteItem(this.id)" width="40px">`
-                );
-
-                newRows.push(tempRow);
-            }
-            dataTable.rows().add(newRows);
-        },
-    });
-}
-
-function moveToInsert() {
-    location.href = "/menuInsert";
-}
 
 function moveToDetail(Idx) {
     const menu_id = Idx;
@@ -62,7 +28,6 @@ function deleteItem(Idx) {
     }
 }
 
-
 // csv 추출
 let columData = [];
 function exportExcel() {
@@ -74,16 +39,26 @@ function exportExcel() {
         for (let j = 0; j < 5; j++) {
             // 처음에 th(컬럼명) 부분 먼저 추출
             if (i == 0 && j == 0) {
-                selectedColum.push(document.getElementsByTagName('th')[0].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[1].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[2].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[3].innerText);
-                selectedColum.push(document.getElementsByTagName('th')[4].innerText);
+                selectedColum.push(
+                    document.getElementsByTagName("th")[0].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[1].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[2].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[3].innerText
+                );
+                selectedColum.push(
+                    document.getElementsByTagName("th")[4].innerText
+                );
             }
-            // url 컬럼일 때 
+            // url 컬럼일 때
             if (j == 2) {
                 // img url 에서 src 내부 url만 추출
-                selectedColum.push((newRows[i][j]).split('"')[3]);
+                selectedColum.push(newRows[i][j].split('"')[3]);
             } else {
                 selectedColum.push(newRows[i][j]);
             }
@@ -97,28 +72,118 @@ function exportExcel() {
     const wb = XLSX.utils.book_new();
     const newWorksheet = excelHandler.getWorksheet();
     XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
-    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), excelHandler.getExcelFileName());
+    const wbout = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+    saveAs(
+        new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
+        excelHandler.getExcelFileName()
+    );
 }
 
 const excelHandler = {
     getExcelFileName: function () {
-        return 'sasohae-menu-data.xlsx';
+        return "sasohae-menu-data.xlsx";
     },
     getSheetName: function () {
-        return 'menuList Sheet';
+        return "menuList Sheet";
     },
     getExcelData: function () {
         return columData;
     },
     getWorksheet: function () {
         return XLSX.utils.aoa_to_sheet(this.getExcelData());
-    }
-}
+    },
+};
 
 function s2ab(s) {
     const buf = new ArrayBuffer(s.length);
     const view = new Uint8Array(buf);
-    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
     return buf;
+}
+
+function checkAdminPosition() {
+    const admin_id = sessionStorage.getItem("_id");
+
+    $.ajax({
+        type: "get",
+        url: `/api/admins/${admin_id}`,
+        data: {},
+        success: (response) => {
+            const adminPosition = response.adminPosition;
+            makeTable(adminPosition);
+        },
+        error: (err) => {
+            alert(err.responseJSON.errorMessage);
+        },
+    });
+}
+
+function makeTable(adminPosition) {
+    const datatablesSimple = document.getElementById("datatablesSimple");
+    const dataTable = new simpleDatatables.DataTable(datatablesSimple);
+
+    $.ajax({
+        type: "get",
+        url: `/api/menus`,
+        data: {},
+        success: (res) => {
+            const allMenus = res;
+
+            for (let i = 0; i < allMenus.length; i++) {
+                let tempRow = [];
+                if (adminPosition != "guest") {
+                    tempRow.push(`${i + 1}`);
+                    tempRow.push(allMenus[i].menuName);
+                    tempRow.push(
+                        `<img width="37px" src="${allMenus[i].menuUrl}">`
+                    );
+                    tempRow.push(allMenus[i].menuRecommendCnt.toString());
+                    tempRow.push(allMenus[i].menuLikeCnt.toString());
+                    tempRow.push(`<input type="button" id="${allMenus[i].menu_id}" onClick="moveToDetail(this.id)"
+                    class="btn btn-outline-primary" value="상세페이지">`);
+                    tempRow.push(
+                        `<img style="cursor:pointer;" src="../public/assets/img/deleteBtn.png" id="${allMenus[i].menu_id}" onClick="deleteItem(this.id)" width="40px">`
+                    );
+                } else {
+                    tempRow.push(`${i + 1}`);
+                    tempRow.push(allMenus[i].menuName);
+                    tempRow.push(
+                        `<img width="37px" src="${allMenus[i].menuUrl}">`
+                    );
+                    tempRow.push(allMenus[i].menuRecommendCnt.toString());
+                    tempRow.push(allMenus[i].menuLikeCnt.toString());
+                    tempRow.push(`<input type="button" id="${allMenus[i].menu_id}" onClick="alert('권한이 필요한 작업입니다!')"
+                    class="btn btn-outline-primary" value="상세페이지">`);
+                    tempRow.push(
+                        `<img style="cursor:pointer;" src="../public/assets/img/deleteBtn.png" id="${allMenus[i].menu_id}" onClick="alert('권한이 필요한 작업입니다!')" width="40px">`
+                    );
+                }
+
+                newRows.push(tempRow);
+            }
+            dataTable.rows().add(newRows);
+        },
+    });
+}
+
+function moveToInsert() {
+    const admin_id = sessionStorage.getItem("_id");
+
+    $.ajax({
+        type: "get",
+        url: `/api/admins/${admin_id}`,
+        data: {},
+        success: (response) => {
+            const adminPosition = response.adminPosition;
+
+            if (adminPosition != "guest") {
+                location.href = "/menuInsert";
+            } else {
+                alert("권한이 필요한 작업입니다!");
+            }
+        },
+        error: (err) => {
+            alert(err.responseJSON.errorMessage);
+        },
+    });
 }
